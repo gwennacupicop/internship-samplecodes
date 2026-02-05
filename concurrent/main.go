794c0cdd71386b9fee3a1037f209a814ba3ea1e9
JovihanniCasenas <casenasjovihanni@gmail.com>
2026-02-05 15:30:18 +0800

    add channel to concurrent sample

--- concurrent/main.go
+++ concurrent/main.go
@@ -7,18 +7,33 @@ import (
 )
 
 var (
-	cc   = flag.Bool("concurrent", false, "If true, run the concurrent function")
+	cc     = flag.Bool("concurrent", false, "If true, run the concurrent function")
+	chFlag = flag.Bool("channel", false, "If true, run the concurrent function with channels")
+	ch     = make(chan string)
+	items  = []string{"task1", "task2", "task3", "task4", "task5"}
 )
 
-func sequential(items []string) {
+func sequential() {
 	for _, item := range items {
+		// Simulate some work with sleep
+		time.Sleep(100 * time.Millisecond)
 		fmt.Println("Printing sequential task: " + item)
 	}
 }
 
-func concurrent(items []string) {
+func concurrent() {
 	for _, item := range items {
 		go func(item string) {
+			// Simulate some work with sleep
+			time.Sleep(100 * time.Millisecond)
+
+			if *chFlag {
+				// send the message to the channel
+				ch <- "Sent task to channel: " + item
+				return
+			}
+
+			// print directly
 			fmt.Println("Printing concurrent task: " + item)
 		}(item)
 	}
@@ -26,16 +41,25 @@ func concurrent(items []string) {
 
 func main() {
 	flag.Parse()
-	items := []string{"task1", "task2", "task3", "task4", "task5"}
-	
+
 	// Log how long did it take.
 	defer func(begin time.Time) {
-		fmt.Println("duration:", time.Since(begin))
+		fmt.Println("Duration:", time.Since(begin))
 	}(time.Now())
 
 	if !*cc {
-		sequential(items)
+		sequential()
 	} else {
-		concurrent(items)
+		concurrent()
+	}
+
+	if !*chFlag {
+		return
+	}
+
+	// receive the message from the channel
+	for range items {
+		msg := <-ch
+		fmt.Println(msg)
 	}
 }
